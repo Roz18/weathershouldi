5fb0f67a46fcaeb84efe2368adf99ac27ea75b99
Roz18 <114506732+Roz18@users.noreply.github.com>
2023-08-09 21:51:59 +0200

    img
    changed img , and add styling

--- src/InfoWeather.js
+++ src/InfoWeather.js
@@ -13,26 +13,28 @@ export default function InfoWeather(props) {
           <FormattedDate date={props.data.date} />
         </ul>
       </div>
+
       <div className="row">
-        <div className="col-3">
-          <div className="tempIcon">
-            <WeatherTemp celsius={props.data.temperature} />
-          </div>
-        </div>
-        <div className="col-3">
+        <div className=" collum col-3">
           <div className="iconMian">
             <Icon
               code={props.data.icon}
               alt={props.data.description}
-              size={45}
+              size={53}
+              className="icon"
             />
           </div>
-          <p className="text-capitalize">{props.data.description}</p>
+        </div>
+        <div className=" collum col-3">
+          <div className="tempIcon">
+            <WeatherTemp celsius={props.data.temperature} />
+          </div>
         </div>
 
         <div className="col-6">
           <div className="Extra">
             <ul>
+              <p className="text-capitalize">{props.data.description}</p>
               <p>Humidity: {props.data.humidity}%</p>
               <p>Wind speed: {props.data.wind} km/h</p>
             </ul>
--- src/WeatherIcons.js
+++ src/WeatherIcons.js
@@ -22,7 +22,21 @@ export default function Icon(props) {
       <ReactSkycon
         icon={codeMapping[props.code]}
         size={props.size}
-        color={{ moon: "pink", light_cloud: "pink" }}
+        color={{
+          sun: "rgb(250, 244, 183)",
+          moon: "rgb(249, 249, 249)",
+          light_cloud: "rgb(221, 230, 237",
+          cloud: "rgb(157, 178, 191)",
+          dark_cloud: "rgb(82, 109, 130)",
+          rain: "rgb(140, 171, 255)",
+          snow: "rgb(241, 240, 232)",
+          thunder: "rgb(238, 224, 201)",
+          wind: "rgb(228, 133, 134)",
+          leaf: "rgb(145, 109, 179)",
+          hail: "rgb(212, 250, 252)",
+          sleet: "rgb(184, 231, 225)",
+          fog: "rgb(221, 230, 237)",
+        }}
       />
     </div>
   );
